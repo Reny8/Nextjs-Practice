87e36e49ee54323b22b500fff712c9ef42638ac3
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2024-03-11 13:03:17 -0400

    Add path helpers + add session to providers.tsx

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@/auth";
 import NavBar from "@/components/NavBar/NavBar";
+import Profile from "@/components/Profile/Profile";
 export default async function Home() {
-  const session = await auth();
   return (
     <div>
       <NavBar auth={auth} />
+      <Profile/>
     </div>
   );
 }
