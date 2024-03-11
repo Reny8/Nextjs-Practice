284818e2262593bf312339ceb9a4ae007c4601aa
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2024-03-11 13:14:24 -0400

    Add pages to each route

--- discuss/src/app/layout.tsx
+++ discuss/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/providers";
-
+import NavBar from "@/components/NavBar/NavBar";
+import { auth } from "@/auth";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar auth={auth}/>
         <Providers>{children}</Providers>
       </body>
     </html>
--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { auth } from "@/auth";
-import NavBar from "@/components/NavBar/NavBar";
 import Profile from "@/components/Profile/Profile";
 export default async function Home() {
   return (
     <div>
-      <NavBar auth={auth} />
       <Profile/>
     </div>
   );
