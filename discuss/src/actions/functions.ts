2e1274ebafec956023a69d4dbafe7474beee7b3c
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2024-03-11 13:58:46 -0400

    Add navbar content

--- discuss/src/actions/functions.ts
+++ discuss/src/actions/functions.ts
@@ -1,7 +1,15 @@
 "use server";
 
-export async function createTopic() {}
+export async function createTopic() {
+    // TODO: Revalidate the following: Home Page
+}
 
-export async function createPost() {}
+export async function createPost() {
+    // TODO: Revalidate the following: Topic Show Page
+    // Time based revalidation for Home Page
+}
 
-export async function createComment() {}
+export async function createComment() {
+    // TODO: Revalidate the following: View a Post Page
+    // Time based revalidation for Topic Show Page, Home Page
+}
--- discuss/src/components/NavBar/NavBar.tsx
+++ discuss/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,15 @@
-import { Button } from "@nextui-org/button";
 import * as actions from "@/actions";
 import "./NavBar.css";
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
 interface NavBarProps {
   auth: () => Promise<any>;
 }
@@ -8,22 +17,26 @@ interface NavBarProps {
 export default async function NavBar(props: NavBarProps) {
   const session = await props.auth();
   return (
-    <nav>
-      <h1>Discuss</h1>
-      <input type="text" placeholder="Search" />
-      {session?.user ? (
-        <form action={actions.signOutAction}>
-          <Button type="submit" className="nav-bar-button">
-            Sign Out
-          </Button>
-        </form>
-      ) : (
-        <form action={actions.signInAction}>
-          <Button type="submit" className="nav-bar-button">
-            Sign In
-          </Button>
-        </form>
-      )}
-    </nav>
+    <Navbar>
+      <Link href="/">
+        <h1>Discuss</h1>
+      </Link>
+      <NavbarContent justify="center">
+        <Input type="text" placeholder="Search" />
+      </NavbarContent>
+      <NavbarContent justify="center">
+        {session?.user ? (
+          <form action={actions.signOutAction}>
+            <Avatar src={session.user.image} />
+          </form>
+        ) : (
+          <form action={actions.signInAction}>
+            <Button type="submit" className="nav-bar-button">
+              Sign In
+            </Button>
+          </form>
+        )}
+      </NavbarContent>
+    </Navbar>
   );
 }
